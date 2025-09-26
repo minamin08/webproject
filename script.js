399bc8b944475f93a3a445d46fc890e19f324d08
minamin08 <91894801+minamin08@users.noreply.github.com>
2025-09-26 14:26:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let menus = document.querySelector(".navbar");
-let menus_btn = document.querySelector(".menu-btn");
-let close_btn = document.querySelector(".close-btn");
+/*===== MENU SHOW Y HIDDEN =====*/ 
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
 
-menus_btn.addEventListener("click", function () {
-  menus.classList.add("active2");
-});
+/*SHOW*/ 
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
 
-close_btn.addEventListener("click", function () {
-  menus.classList.remove("active2");
-});
+/*HIDDEN*/
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
